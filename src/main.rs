3dc586d9c6ce6e1e77317f6c249e007d4562ba51
Ian Elizondo <ianeli@microsoft.com>
2022-05-17 20:09:45 -0600

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use derive_more::From;
+use image;
+use reqwest;
+use std::io::{Cursor, Read, Seek, SeekFrom};
+use std::time::Instant;
+use timing::measure_duration;
+
+#[macro_use]
+extern crate rocket;
+
+const CAT_URL: &'static str = "https://images.unsplash.com/photo-1611915387288-fd8d2f5f928b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2F0JTIwZXllc3xlbnwwfHwwfHw%3D&w=1000&q=80";
+
+#[derive(From, Debug)]
+enum GetImageError {
+    ImagesError(image::ImageError),
+    ReqwestError(reqwest::Error),
+}
+
+async fn get_image<'r>(url: &str) -> Result<image::DynamicImage, GetImageError> {
+    let start = Instant::now();
+    let bytes = reqwest::get(url).await?.bytes().await?;
+    println!("Download duration: {:?}", start.elapsed());
+
+    Ok(image::load_from_memory(&bytes)?)
+}
+
+#[measure_duration]
+fn image_to_png(image: image::DynamicImage) -> Vec<u8> {
+    let mut buf = Cursor::new(Vec::new());
+
+    image
+        .write_to(&mut buf, image::ImageOutputFormat::Png)
+        .expect("An error ocurred!");
+
+    buf.seek(SeekFrom::Start(0)).expect("An error ocurred");
+
+    let mut out = Vec::new();
+    buf.read_to_end(&mut out)
+        .expect("An error ocurred copying the cursor");
+
+    return out;
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello world"
+}
+
+#[derive(Responder, Debug, Clone)]
+enum CatResponses<'r> {
+    #[response(status = 200, content_type = "image/png")]
+    ImageRes(Vec<u8>),
+    #[response(status = 500)]
+    Failure(&'r str),
+}
+
+#[get("/cat")]
+async fn cat_endpoint<'a>() -> CatResponses<'a> {
+    if let Ok(cat) = get_image(CAT_URL).await {
+        return CatResponses::ImageRes(image_to_png(cat));
+    } else {
+        return CatResponses::Failure("An error ocurred");
+    }
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/", routes![cat_endpoint])
+}
---
+++ timing/src/lib.rs
@@ -0,0 +1,33 @@
+extern crate proc_macro;
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::parse_macro_input;
+use syn::ItemFn;
+
+//my first macro!!
+
+//attr is whatever you pass to the macro itself
+//item is the attribute this macro is being called on
+#[proc_macro_attribute]
+pub fn measure_duration(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input_fn = parse_macro_input!(item as ItemFn);
+    let fn_name = input_fn.sig.ident;
+    let fn_inputs = input_fn.sig.inputs;
+    let fn_body = input_fn.block;
+    let fn_return = input_fn.sig.output;
+    let fn_name_str = format!("{}", fn_name);
+    let tokens = quote! {
+        fn #fn_name(#fn_inputs) #fn_return {
+            let _start = Instant::now();
+            let _in = ||
+                #fn_body
+
+
+            ;println!("Duration of {} : {:?}", #fn_name_str, _start.elapsed());
+
+            _in()
+        }
+    };
+
+    TokenStream::from(tokens)
+}
